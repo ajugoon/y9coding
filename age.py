33144668e9f2bbdd15ad47b54bc14cc1f3cd9230
ajugoon <54783263+ajugoon@users.noreply.github.com>
2020-12-05 23:23:12 -0500

    Delete age.py

--- age.py
+++
@@ -1,7 +0,0 @@
-# Please add some comments!
-
-name = input("What's your name? ")
-print("Nice to meet you " + name + "!")
-year = input("What year were you born? ")
-age = 2020 - int(year)
-print("You are " + str(age) + " years old, " + name + "!")
