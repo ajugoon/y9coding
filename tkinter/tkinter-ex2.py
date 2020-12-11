4358e4423dbc5784be3e0f309e2d139ef1b441ae
ajugoon <54783263+ajugoon@users.noreply.github.com>
2020-12-10 19:34:29 -0500

    Delete tkinter-ex2.py

--- tkinter/tkinter-ex2.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-MyWindow = Tk()
-MyWindow.title("Welcome to UCC!")
-MyWindow.geometry('350x200')
-lbl = Label(MyWindow, text="Hello")
-lbl.grid(column=0, row=0)
-btn = Button(MyWindow, text="Click Me")
-btn.grid(column=1, row=0)
-MyWindow.mainloop()
